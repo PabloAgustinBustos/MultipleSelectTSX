a692c2c40a3e4d7b58fe2c13cc774d0f5e2828e7
Pablo Bustos <pablobustos609@gmail.com>
2023-01-24 23:01:09 -0300

    structure done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import { useState } from 'react'
+import Select from './Select'
+
+const options = [
+  {
+    label: "First",
+    value: 1
+  },
+
+  {
+    label: "Second",
+    value: 2
+  },
+  {
+    label: "Third",
+    value: 3
+  },
+  {
+    label: "Four",
+    value: 4
+  },
+]
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hola</h1>
-    </div>
-  )
+  return (<>
+    <Select options={options}/>
+  </>)
 }
 
 export default App
--- src/Select.tsx
+++ src/Select.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import s from "./Select.module.css"
 
 type SelectOption = {
   label: string
-  value: string
+  value: any
 }
 
 type SelectProps = {
@@ -13,7 +14,19 @@ type SelectProps = {
 
 const Select = ({value, onChange, options}: SelectProps) => {
   return (
-    <div>Select</div>
+    <div className={s.container}>
+      <span className={s.value}>Value</span>
+      <button className={s["clear-btn"]}>&times;</button>
+
+      <div className={s.divider}></div>
+      <div className={s.caret}></div>
+
+      <ul className={s.options}>
+        {options.map(op => (
+          <li key={op.label} className={s.option}>{op.label}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
